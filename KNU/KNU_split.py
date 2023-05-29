6298adc26c5d27c69bbfdddb0a7b812a1c392c6e
TTobe <35289282+TTobe@users.noreply.github.com>
2023-05-29 15:25:23 +0900

    5/29

--- KNU/KNU_split.py
+++ KNU/KNU_split.py
@@ -5,21 +5,18 @@ standard = os.listdir('data')
 standard.sort()
 print(standard)
 
-path = 'KnuSentiLex-master/data/SentiWord_info.json'
+path = 'KNU/KnuSentiLex-master/data/SentiWord_info.json'
 
 with open(path, 'r', encoding="UTF-8") as file:
     data = json.load(file)
     
 # 14853 {'word': '알쏭달쏭하다', 'word_root': '알쏭달쏭', 'polarity': '-1'}
 for i in range(100):
-    print(i)
-    print(data[i]['word'])
-    print(data[i]['word_root'])
+    print('몇번째: ',i)
+    print('단어:\t',data[i]['word'])
+    print('단어 root:',data[i]['word_root'])
+    print('극성', data[i]['polarity'])
     choose = int(input('어디로 분류: '))
-    
-    
-
-
-    
+    plor = int(input('극성 입력: '))
 
     
